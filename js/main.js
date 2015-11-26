04c48e69bee10d01d6d5754975bc4805baa2ac03
Jigsaw <cupuycblack94@gmail.com>
2015-11-26 19:14:25 +0200

    finishing scratch card

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
-	$('#elem').wScratchPad({
-	  size        : 150,          // The size of the brush/scratch.
-	  bg          : 'img/bg.jpg',  // Background (image path or hex color).
-	  fg          : 'img/ScratchImage.jpg',  // Foreground (image path or hex color).
-	  realtime    : true,       // Calculates percentage in realitime.
-	  scratchDown: function(e, percent){ },
-  	scratchMove: function(e, percent){ },
-  	scratchUp: function(e, percent){  },
-	  cursor      : null // Set cursor.
-	});  
+  $('#elem').wScratchPad({
+    size        : 50,          // The size of the brush/scratch.
+    bg          : 'css/img/bg.jpg',  // Background (image path or hex color).
+    fg          : 'css/img/ScratchImage.jpg',  // Foreground (image path or hex color).
+    realtime    : true,       // Calculates percentage in realitime.
+    scratchDown : function(e, percent){ },
+    scratchMove : function(e, percent){
+      if(percent>75){
+        $('#elem').css('display', 'none');
+        $('#send-form').fadeIn();
+      }
+      },
+    scratchUp   : function(e, percent){  },
+    cursor      : null // Set cursor.
+  });
+  $('#send').on('click', function(){
+    $('#thanx-msg').fadeIn();
+    $('#send-form').css('display', 'none');
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: {
+        email:$('#email').val(),
+        name:$('#name').val(),
+        phone:$('#phone').val(),
+        checkbox:$('#checkbox').is(":checked")
+      },
+      dataType: 'json'
+    });
+  });  
 });
